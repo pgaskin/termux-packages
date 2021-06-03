b2f573a768930c3d8033ae41ebb9c1050e7baf7e
Patrick Gaskin <patrick@pgaskin.net>
2021-06-03 17:19:28 -0400

    cmus: pgaskin/cmus@71ebcd382d20f1fdb06aa8cded21955caf08a80e (exp)

--- packages/cmus/build.sh
+++ packages/cmus/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://cmus.github.io/
 TERMUX_PKG_DESCRIPTION="Small, fast and powerful console music player"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.9.1~exp.a3e6caa
+TERMUX_PKG_VERSION=2.9.1~exp.71ebcd3
 TERMUX_PKG_DEPENDS="libandroid-support, libiconv, ncurses, pulseaudio, ffmpeg, libmad, opusfile, libflac, libvorbis"
 TERMUX_PKG_SRCURL=https://github.com/pgaskin/cmus/archive/${TERMUX_PKG_VERSION##*.}.tar.gz
-TERMUX_PKG_SHA256=ba4543fa2e88e0f04e285cf9f8c5031d0fe09f2795196b3f9deec5a8b627e25d
+TERMUX_PKG_SHA256=a791355a1b459d3f630653e937ebfb4743ab2b10089f14b5f88ce79051fb4f56
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
