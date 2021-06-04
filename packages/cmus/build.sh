c043c1bcddf8af6c5dd6ec1280818fab1fb01af7
Patrick Gaskin <patrick@pgaskin.net>
2021-06-04 12:32:42 -0400

    cmus: Update maintainer and version format

--- packages/cmus/build.sh
+++ packages/cmus/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://cmus.github.io/
 TERMUX_PKG_DESCRIPTION="Small, fast and powerful console music player"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.9.1~exp.45c5a31
+TERMUX_PKG_MAINTAINER="@pgaskin"
+TERMUX_PKG_VERSION=2.9.1+git20210604.exp.45c5a31
 TERMUX_PKG_DEPENDS="libandroid-support, libiconv, ncurses, pulseaudio, ffmpeg, libmad, opusfile, libflac, libvorbis"
 TERMUX_PKG_SRCURL=https://github.com/pgaskin/cmus/archive/${TERMUX_PKG_VERSION##*.}.tar.gz
 TERMUX_PKG_SHA256=b9043a69f05b464a943d3e3626e995a278e6ac1f062242100f67742410ab510c
